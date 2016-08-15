294eac20645f66ff0346f4fc0fe24a734435c4ce
Héctor Menéndez <etor.mx@gmail.com>
2016-08-15 08:10:08 -0500

    Updated test cases.

---
+++ test/app/method-invalid/routes.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    '/': { bundle:'root', method:123 }
+}
--- test/index.js
+++ test/index.js
@@ -2,13 +2,32 @@
 
 const Tape  = require('tape');
 const Feliz = require('feliz');
-const Observable = Feliz.observable;
 
-const Router = require('../lib/router');
-const tests = require('./cases');
+const router     = require('../lib/router');
+const tests      = require('./cases');
+const observable = Feliz.observable;
+
+const Conf = {
+    plugins: [router],
+    // Tape executable (mainModule) doesn't have an extension,
+    // So feliz doesn't get one by default. fixed it.
+    path : { ext: { type:'extname', args:[__filename] } }
+};
 
 Tape('The returned feliz observable', t => {
-    const onInstance = test => {
+
+    const test$ = tests.stream().concatMap(test => {
+        const conf = Object.assign({}, test.conf, Conf);
+        const feliz$ =  new Feliz(conf);
+
+        return feliz$
+            .catch(err => observable.of(err))
+            .map(out => Object.assign({out}, test))
+    });
+
+    test$.subscribe(onInstance, onError, () => t.end());
+
+    function onInstance(test){
         const t1 = test.out instanceof Error;
         const t2 = test.out.constructor.name === 'Feliz';
         const m1 = `should ${test.pass? 'not':''} stream error when ${test.desc}`;
@@ -18,26 +37,17 @@ Tape('The returned feliz observable', t => {
             t.equal(t1, false, m1);
             t.equal(t2, true, m2);
             // unexpected error
-            if (t1 !== false) console.log(test.out);
+            if (t1 !== false) console.error(test.out);
         } else {
             // should stream and error and not return a feliz instance
             t.equal(t1, true, m1);
             t.equal(t2, false, m2);
-            // unexpected instance
-            if (t1 !== true) console.log(test.out.conf);
         }
         if (test.cbak) test.cbak(t, test);
     };
-    const onError = error => {
+
+    function onError(error){
         t.fail('should never show this message while testing');
-        console.log(error);
+        console.error(error);
     };
-    const onEnd = () => t.end();
-    const tests$ = tests
-        .stream()
-        .concatMap(test => (new Feliz(Object.assign({plugins:[Router]}, test.conf)))
-            .map(feliz => Object.assign({out:feliz}, test))
-            .catch(error => Observable.of(Object.assign({out: error}, test)))
-        )
-        .subscribe(onInstance, onError, onEnd);
 });
